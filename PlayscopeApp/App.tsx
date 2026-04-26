7f9ce84fab3272d90efbf73980b8f2e95586e909
Felipe <fcsantos2201@gmail.com>
2026-04-26 17:48:58 -0300

    feat: estrutura do app e telas de login/home finalizadas

---
+++ PlayscopeApp/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import AppNavigator from './src/navigation/AppNavigator';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="#000" />
+      <AppNavigator />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+});
+
+export default App;
